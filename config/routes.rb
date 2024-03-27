f009c5156b58e4995977d594f2803d6bc94fae4d
Nathan Peters <peters.nate8@gmail.com>
2024-03-27 16:22:54 -0600

    define routes, create job model, add attributes into jobs table

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # post request endpoint to add job to database
+  post "/jobs" => "jobs#create"
+
+  # get request endpoint to get all jobs from database
+  get "/jobs" => "jobs#index"
+
+  # get request endpoint to get job by id from database
+  get "/jobs/:id" => "jobs#show"
+  
 end
